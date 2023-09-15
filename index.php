ce122ba665fc947446fc61735ceb6cfc19623824
DimaDimaDima94 <143801671+DimaDimaDima94@users.noreply.github.com>
2023-09-15 12:41:55 +0300

    Создание коммита Должно быть с комментом Выражать определенную зафиксированную функцию

--- index.php
+++ index.php
@@ -5,10 +5,7 @@
 <head>
 <link rel ="stylesheet" href="css/style.min.css">
 
-<style>
-   /* body {
-        background: #363F46;
-} */
+
   </style>
 
 <meta charset="UTF-8">
@@ -21,12 +18,12 @@
 
 <!-- <img src="img/desctop_carcas.png" width="1440" height="5990"  alt=""> -->
 
-<body >
+<body>
 
 
 <table>
     <tr>  
-        <td colspan="1"> <img no-repeat src="img/desctop_carcas.png" width="100%" alt=""> </td>
+        <!-- <td  colspan="1"> <img no-repeat src="img/desctop_carcas.png" width="100%" alt=""> </td> -->
     </tr>
 </table>
 
