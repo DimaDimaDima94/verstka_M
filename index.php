1fecf0c873871f92edeed625f1e246dd9355af19
DimaDimaDima94 <143801671+DimaDimaDima94@users.noreply.github.com>
2023-09-15 12:30:41 +0300

    Создание коммита Должно быть с комментом Выражать определенную зафиксированную функцию

--- index.php
+++ index.php
@@ -3,20 +3,34 @@
 <html lang="ru">
 
 <head>
+<link rel ="stylesheet" href="css/style.min.css">
+
+<style>
+   /* body {
+        background: #363F46;
+} */
+  </style>
 
 <meta charset="UTF-8">
 
-<title>Пример веб-страницы с php кодом</title>
+<title>Веб-страница с php кодом</title>
 
 </head>
 
-<body>
 
-<?
 
-echo 'ПРИВЕТ МИР f2443';
+<!-- <img src="img/desctop_carcas.png" width="1440" height="5990"  alt=""> -->
 
-?>
+<body >
+
+
+<table>
+    <tr>  
+        <td colspan="1"> <img no-repeat src="img/desctop_carcas.png" width="100%" alt=""> </td>
+    </tr>
+</table>
+
+
 
 </body>
 
